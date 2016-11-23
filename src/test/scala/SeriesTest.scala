700e1252a7af4c6cb423817085ad237d55b5b527
Karn <nattakarn.kl@gmail.com>
2016-11-23 17:06:36 +0700

    testcase for plusminus

--- src/test/scala/SeriesTest.scala
+++ src/test/scala/SeriesTest.scala
@@ -2,26 +2,61 @@ import io.muic.scandas.core.ScException.DiffDimException
 import io.muic.scandas.core.Series
 import org.scalatest.{FlatSpec, Matchers}
 
-class SeriesTest extends FlatSpec with Matchers{
+import scala.collection.mutable
+
+class SeriesTest extends FlatSpec with Matchers {
 
   "A series" should "do a lot of stuff such as element wise stuff" in {
-    val d = Vector(1.0,5.0,7.0,4.0,10.0)
+    val d = Vector(1.0, 5.0, 7.0, 4.0, 10.0)
     val d2 = Vector(2.0, 3.0, 5.0, 7.0, 9.0)
-    val d3 = Vector(-1.0, 2.0, -3.0, 4.0)
     val ts1 = new Series(d)
     val ts2 = new Series(d2)
-    val ts3 = new Series(d3)
-
-    ts1.toVector should be (d)
-    ts2.toVector should be (d2)
-    (ts1 + ts2).toVector should be (Vector(3.0, 8.0, 12.0, 11, 19))
-//    (ts1 * ts2).toVector should be (Vector(2.0, 15.0, 35.0, 28.0, 90.0))
-    ts3.abs.toVector should be (Vector(1.0, 2.0, 3.0, 4.0 ))
-    a [DiffDimException] should be thrownBy (ts1 + ts3)
+    ts1.toVector should be(d)
+    ts2.toVector should be(d2)
   }
 
-  "A series" should "do boolean indexing too" in {
-    val d = Array(1.0, 2.0 ,3.0, 4.0, 5.0)
+  it should "become absolute number" in {
+    val d = Vector(-1.0, 2.0, -3.0, 4.0, -5.0)
+    val ts1 = new Series(d)
+    ts1.abs.toVector should be(Vector(1.0, 2.0, 3.0, 4.0, 5.0))
   }
+
+  it should "plus each series together" in {
+    val d = Vector(1.0, 5.0, 7.0, 4.0, 10.0)
+    val d2 = Vector(-1.0, 2.0, -3.0, 4.0, -5.0)
+    val ts1 = new Series(d)
+    val ts2 = new Series(d2)
+    (ts1 + ts2).toVector should be(Vector(0.0, 7.0, 4.0, 8.0, 5.0))
+  }
+
+  it should "minus each other" in {
+    val d = Vector(1.0, 5.0, 7.0, 4.0, 10.0)
+    val d2 = Vector(-1.0, 2.0, -3.0, 4.0, -5.0)
+    val ts1 = new Series(d)
+    val ts2 = new Series(d2)
+    (ts1 - ts2).toVector should be(Vector(-2.0, 3.0, 10.0, 0.0, 15.0))
+  }
+
+  it should "multiply each other" in {
+    val d = Vector(1.0, 5.0, 7.0, 4.0, 10.0)
+    val d2 = Vector(-1.0, 2.0, -3.0, 4.0, -5.0)
+    val ts1 = new Series(d)
+    val ts2 = new Series(d2)
+    (ts1 * ts2).toVector should be(Vector(-1.0, 10.0, -21.0, 16.0, -50.0))
+  }
+
+  it should "divide series A by series B" in {
+    val d = Vector(1.0, 10.0, 7.0, 4.0, 0.0)
+    val d2 = Vector(-1.0, 2.0, -2.0, 0.0, -5.0)
+    val ts1 = new Series(d)
+    val ts2 = new Series(d2)
+    (ts1 / ts2).toVector should be(Vector(-1.0, 5.0, -3.5, Double.NaN, 0.0))
+  }
+
+
 
 }
+
+
+
+

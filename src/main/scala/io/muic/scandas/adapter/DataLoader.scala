1d87b1bb6b427b9bafd80b39b410d8d0214997d4
May <calopc.canova@gmail.com>
2016-11-23 17:46:36 +0700

    ปฐมบทcommit

--- src/main/scala/io/muic/scandas/adapter/DataLoader.scala
+++ src/main/scala/io/muic/scandas/adapter/DataLoader.scala
@@ -1,9 +1,9 @@
 package io.muic.scandas.adapter
 
 class DataLoader {
+  // URL = path/url
+  def readCSV(URL: String): Map[Int,Vector[T]] = ??? //read apache -> turn string to be map of vector
 
-  def readCSV(file: String): Unit = ???
-
-  def readJSON(file: String): Unit = ???
+  def readJSON(URL: String): Map[Int,Vector[T]] = ??? // turn string to be map of vector
 
 }

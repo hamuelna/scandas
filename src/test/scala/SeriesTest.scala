f984bbdd012ddfeb58b0f522c6d4ad175ea066b7
Karn <nattakarn.kl@gmail.com>
2016-11-23 17:20:20 +0700

    เพิ่มเติมบวกลบคูณหาร

--- src/test/scala/SeriesTest.scala
+++ src/test/scala/SeriesTest.scala
@@ -24,33 +24,51 @@ class SeriesTest extends FlatSpec with Matchers {
   it should "plus each series together" in {
     val d = Vector(1.0, 5.0, 7.0, 4.0, 10.0)
     val d2 = Vector(-1.0, 2.0, -3.0, 4.0, -5.0)
+    val d3 = Vector(1, 2, 3, 4)
+    val d4 = Vector(3, 2, 5, 6)
+    val d5 = Vector(3, 2, 1)
     val ts1 = new Series(d)
     val ts2 = new Series(d2)
+    val ts3 = new Series(d3)
+    val ts4 = new Series(d4)
+    val ts5 = new Series(d5)
     (ts1 + ts2).toVector should be(Vector(0.0, 7.0, 4.0, 8.0, 5.0))
+    (ts3 + ts4).toVector should be(Vector(4, 4, 8, 10))
+    (ts3 + ts5).toVector should be(Vector(6.0, 4.0, 6.0, Double.NaN))
+
   }
 
   it should "minus each other" in {
     val d = Vector(1.0, 5.0, 7.0, 4.0, 10.0)
     val d2 = Vector(-1.0, 2.0, -3.0, 4.0, -5.0)
+    val d3 = Vector(3, 6, 5)
     val ts1 = new Series(d)
     val ts2 = new Series(d2)
+    val ts3 = new Series(d3)
     (ts1 - ts2).toVector should be(Vector(-2.0, 3.0, 10.0, 0.0, 15.0))
+    (ts2 - ts3).toVector should be(Vector(-4.0, -4.0, -8.0, Double.NaN, Double.NaN))
   }
 
   it should "multiply each other" in {
     val d = Vector(1.0, 5.0, 7.0, 4.0, 10.0)
     val d2 = Vector(-1.0, 2.0, -3.0, 4.0, -5.0)
+    val d3 = Vector(3, 6, 5)
     val ts1 = new Series(d)
     val ts2 = new Series(d2)
+    val ts3 = new Series(d3)
     (ts1 * ts2).toVector should be(Vector(-1.0, 10.0, -21.0, 16.0, -50.0))
+    (ts2 * ts3).toVector should be(Vector(-3.0, 12.0, -15.0, Double.NaN, Double.NaN))
   }
 
   it should "divide series A by series B" in {
     val d = Vector(1.0, 10.0, 7.0, 4.0, 0.0)
     val d2 = Vector(-1.0, 2.0, -2.0, 0.0, -5.0)
+    val d3 = Vector(3, 6, 7)
     val ts1 = new Series(d)
     val ts2 = new Series(d2)
+    val ts3 = new Series(d3)
     (ts1 / ts2).toVector should be(Vector(-1.0, 5.0, -3.5, Double.NaN, 0.0))
+    (ts3 / ts1).toVector should be(Vector(3.0, 0.6, 1.0, Double.NaN, Double.NaN))
   }
 
 
